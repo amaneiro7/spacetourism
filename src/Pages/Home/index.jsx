e9276e6ca3133d8077619e1a8b39d30eefb86714
Andres Maneiro <amaneiro7@gmail.com>
2023-05-07 23:40:54 -0400

    button action

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Layout } from '../../components/Layout'
 import { Article, Button, Wrapper } from './style'
+import { useNavigate } from 'react-router-dom'
 
 export const HomePage = () => {
+  const naviate = useNavigate()
   return (
     <Layout page='homepage'>
       <Wrapper>
@@ -11,7 +13,7 @@ export const HomePage = () => {
           <h1>SPACE</h1>
           <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
         </Article>
-        <Button type='button'><h4>Explore</h4></Button>
+        <Button type='button' onClick={() => naviate('/destination')}><h4>Explore</h4></Button>
       </Wrapper>
     </Layout>
   )
